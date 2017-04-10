b8bcba1a2e97f0dbbc46b16edd41d3d9c2f8e865
Legoman99573 <christianwhead@gmail.com>
2017-04-10 14:10:39 -0500

    test

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
-$(".icons").load("url("AppQR.html")");
+//fa-soundcloud refers to which font-awesome icon to use
+var myicon= new trayItem("fa-soundcloud", "myicon");
+function myicon() {
+   alert("yey you clicked it!");
+
+   //remove the icon
+   myicon.destroy()
+}
